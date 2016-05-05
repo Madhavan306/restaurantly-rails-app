f45bbcc50f865b5cb9765c56ea85727277c7d949
Madhavan Nagarajan <madhavannagarajan@outlook.com>
2016-05-05 21:01:09 +0530

    added resttaurants controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Restaurantly::Application.routes.draw do
+ resources :restaurants
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
