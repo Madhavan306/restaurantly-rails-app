531eb4dab9c25624107e437ad5a09c069203efaf
Madhavan Nagarajan <madhavannagarajan@outlook.com>
2016-05-06 20:04:52 +0530

    creation & listing of restaurants

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,14 +1,25 @@
 class RestaurantsController < ApplicationController
   def show
+    @restaurant = Restaurant.find(params[:id])
+    
   end
 
   def index
+    @restaurant = Restaurant.all
   end
 
   def new
+@restaurant = Restaurant.new
   end
 
   def create
+    restaurant = Restaurant.new(restaurant_p)
+     restaurant.save
+    redirect_to restaurant_path(restaurant.id)
+  end
+private
+  def restaurant_p
+    params.require(:restaurant).permit(:name ,:address)
   end
 
   def edit
